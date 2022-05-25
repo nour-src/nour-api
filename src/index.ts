599db8cf774a612dd66194669c34483184d8bb76
Abdelrhman Said <abdelrhmansaidzaki@hotmail.com>
2022-05-25 21:42:40 +0200

    Initial Commit

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import express, { Request, Response } from 'express'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const app = express()
+const PORT = process.env.PORT || 3000
+
+// Handle not found routes
+app.use((_req: Request, res: Response) => {
+    res.status(404).json({
+        code: 404,
+        message: 'route not found',
+    })
+})
+
+app.listen(PORT, () => {
+    console.log(`⚡️ Server is running on port ${PORT}`)
+})
