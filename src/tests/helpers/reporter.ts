104e212bae5d85b12550565960aed882d3431007
Abdelrhman Said <abdelrhmansaidzaki@hotmail.com>
2022-05-28 20:36:20 +0200

    Optimized Jasmine Reporter

--- src/tests/helpers/reporter.ts
+++ src/tests/helpers/reporter.ts
@@ -1,15 +1,4 @@
-import {
-  DisplayProcessor,
-  SpecReporter,
-  StacktraceOption,
-} from 'jasmine-spec-reporter'
-import SuiteInfo = jasmine.SuiteInfo
-
-class CustomProcessor extends DisplayProcessor {
-  public displayJasmineStarted(info: SuiteInfo, log: string): string {
-    return `TypeScript ${log}`
-  }
-}
+import { SpecReporter, StacktraceOption } from 'jasmine-spec-reporter'
 
 jasmine.getEnv().clearReporters()
 jasmine.getEnv().addReporter(
@@ -17,6 +6,5 @@ jasmine.getEnv().addReporter(
     spec: {
       displayStacktrace: StacktraceOption.NONE,
     },
-    customProcessors: [CustomProcessor],
   })
 )
